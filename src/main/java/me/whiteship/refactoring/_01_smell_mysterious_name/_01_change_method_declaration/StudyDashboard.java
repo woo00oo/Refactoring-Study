6e24d27b709a1a93ffb4722e18add7be4e658ee5
woo00oo <96woo94@naver.com>
2022-08-04 22:20:20 +0900

    리팩토링 1. 함수 선언 변경하기

--- src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_01_change_method_declaration/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_01_change_method_declaration/StudyDashboard.java
@@ -16,7 +16,14 @@ public class StudyDashboard {
 
     private Set<String> reviews = new HashSet<>();
 
-    private void studyReviews(GHIssue issue) throws IOException {
+    /**
+     * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
+     * @throws IOException
+     */
+    private void loadReviews() throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(30);
         List<GHIssueComment> comments = issue.getComments();
         for (GHIssueComment comment : comments) {
             usernames.add(comment.getUserName());
@@ -33,13 +40,22 @@ public class StudyDashboard {
     }
 
     public static void main(String[] args) throws IOException {
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
-
         StudyDashboard studyDashboard = new StudyDashboard();
-        studyDashboard.studyReviews(issue);
+        studyDashboard.loadReviews();
         studyDashboard.getUsernames().forEach(System.out::println);
         studyDashboard.getReviews().forEach(System.out::println);
     }
 }
+
+/**
+ * 리팩토링 1. 함수 선언 변경하기
+ * > 함수 이름 변경하기, 메소드 이름 변경하기, 매개변수 추가하기, 매개변수 제거하기, 시그니처 변경하기
+ *
+ * - 좋은 이름을 가진 함수는 함수가 어떻게 구현되었는지 코드를 보지 않아도 이름만 보고도 이해할 수 있다.
+ *
+ * - 좋은 이름을 찾아내는 방법? 함수에 주석을 작성한 다음, 주석을 함수 이름으로 만들어 본다.
+ *
+ * - 함수의 매개변수는
+ *  - 함수 내부의 문맥을 결정한다. (예, 전화번호 포매팅 함수)
+ *  - 의존성을 결정한다. (예, Payment 만기일 계산 함수)
+ */
