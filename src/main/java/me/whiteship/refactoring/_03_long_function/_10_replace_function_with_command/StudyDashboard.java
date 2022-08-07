fc285771161121694f0cc8e36618b035b7247b26
woo00oo <96woo94@naver.com>
2022-08-07 19:51:59 +0900

    리팩토링 10. 함수를 명령으로 바꾸기

--- src/main/java/me/whiteship/refactoring/_03_long_function/_10_replace_function_with_command/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_10_replace_function_with_command/StudyDashboard.java
@@ -5,10 +5,7 @@ import org.kohsuke.github.GHIssueComment;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
@@ -26,17 +23,6 @@ public class StudyDashboard {
     public static void main(String[] args) throws IOException, InterruptedException {
         StudyDashboard studyDashboard = new StudyDashboard(15);
         studyDashboard.print();
-    }
-
-    private Participant findParticipant(String username, List<Participant> participants) {
-        Participant participant = null;
-        if (participants.stream().noneMatch(p -> p.username().equals(username))) {
-            participant = new Participant(username);
-            participants.add(participant);
-        } else {
-            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
-        }
-        return participant;
     }
 
     private void print() throws IOException, InterruptedException {
@@ -72,55 +58,37 @@ public class StudyDashboard {
         latch.await();
         service.shutdown();
 
-        try (FileWriter fileWriter = new FileWriter("participants.md");
-            PrintWriter writer = new PrintWriter(fileWriter)) {
-            participants.sort(Comparator.comparing(Participant::username));
+        new StudyPrinter(this.totalNumberOfEvents, participants).execute();
 
-            writer.print(header(participants.size()));
-
-            participants.forEach(p -> {
-                String markdownForHomework = getMarkdownForParticipant(p);
-                writer.print(markdownForHomework);
-            });
-        }
     }
 
-    private String getMarkdownForParticipant(Participant p) {
-        return String.format("| %s %s | %.2f%% |\n", p.username(), checkMark(p, this.totalNumberOfEvents),
-                p.getRate(this.totalNumberOfEvents));
-    }
-
-    /**
-     * | 참여자 (420) | 1주차 | 2주차 | 3주차 | 참석율 |
-     * | --- | --- | --- | --- | --- |
-     */
-    private String header(int totalNumberOfParticipants) {
-        StringBuilder header = new StringBuilder(String.format("| 참여자 (%d) |", totalNumberOfParticipants));
-
-        for (int index = 1; index <= this.totalNumberOfEvents; index++) {
-            header.append(String.format(" %d주차 |", index));
+    private Participant findParticipant(String username, List<Participant> participants) {
+        Participant participant = null;
+        if (participants.stream().noneMatch(p -> p.username().equals(username))) {
+            participant = new Participant(username);
+            participants.add(participant);
+        } else {
+            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
         }
-        header.append(" 참석율 |\n");
-
-        header.append("| --- ".repeat(Math.max(0, this.totalNumberOfEvents + 2)));
-        header.append("|\n");
-
-        return header.toString();
-    }
-
-    /**
-     * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
-     */
-    private String checkMark(Participant p, int totalEvents) {
-        StringBuilder line = new StringBuilder();
-        for (int i = 1 ; i <= totalEvents ; i++) {
-            if(p.homework().containsKey(i) && p.homework().get(i)) {
-                line.append("|:white_check_mark:");
-            } else {
-                line.append("|:x:");
-            }
-        }
-        return line.toString();
+        return participant;
     }
 
 }
+
+/**
+ * 함수를 명령으로 바꾸기
+ *
+ * - 함수를 독립적인 객체인, Command로 만들어 사용할 수 있다.
+ *
+ * - 커맨드 패턴을 적용하면 다음과 같은 장점을 취할 수 있다.
+ *
+ *  - 부가적인 기능으로 undo 기능을 만들 수도 있다.
+ *
+ *  - 더 복잡한 기능을 구현하는데 필요한 여러 메소드를 추가할 수 있다.
+ *
+ *  - 상속이나 템플릿을 활용할 수도 있다.
+ *
+ *  - 복잡한 메소드를 여러 메소드나 필드를 활용해 쪼갤 수도 있다.
+ *
+ * - 대부분의 경우에 "커맨드" 보다는 "함수"를 사용하지만, 커맨드 발고 다른 방법이 없는 경우에만 사용한다.
+ */
