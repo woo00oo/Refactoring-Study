ac141a99f0f0ded8329148d0c8e6d9160239ea84
woo00oo <96woo94@naver.com>
2022-08-10 22:35:27 +0900

    리팩토링 15. 플래그 인수 제거하기

--- src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_15_remove_flag_argument/Shipment.java
+++ src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_15_remove_flag_argument/Shipment.java
@@ -4,21 +4,32 @@ import java.time.LocalDate;
 
 public class Shipment {
 
-    public LocalDate deliveryDate(Order order, boolean isRush) {
-        if (isRush) {
-            int deliveryTime = switch (order.getDeliveryState()) {
-                case "WA", "CA", "OR" -> 1;
-                case "TX", "NY", "FL" -> 2;
-                default -> 3;
-            };
-            return order.getPlacedOn().plusDays(deliveryTime);
-        } else {
-            int deliveryTime = switch (order.getDeliveryState()) {
-                case "WA", "CA" -> 2;
-                case "OR", "TX", "NY" -> 3;
-                default -> 4;
-            };
-            return order.getPlacedOn().plusDays(deliveryTime);
-        }
+    public LocalDate regularDeliveryDate(Order order) {
+        int deliveryTime = switch (order.getDeliveryState()) {
+            case "WA", "CA" -> 2;
+            case "OR", "TX", "NY" -> 3;
+            default -> 4;
+        };
+        return order.getPlacedOn().plusDays(deliveryTime);
+    }
+
+    public LocalDate rushDeliveryDate(Order order) {
+        int deliveryTime = switch (order.getDeliveryState()) {
+            case "WA", "CA", "OR" -> 1;
+            case "TX", "NY", "FL" -> 2;
+            default -> 3;
+        };
+        return order.getPlacedOn().plusDays(deliveryTime);
     }
 }
+
+/**
+ * 플래그 인수 제거하기
+ *
+ * - 플래그는 보통 함수에 매개변수로 전달해서, 함수 내부의 로직을 분기하는데 사용한다.
+ *
+ * - 플래그를 사용한 함수는 차이를 파악하기 어렵다.
+ *
+ * - 조건문 분해하기(Decompose Condition)를 활용할 수 있다.
+ *
+ */
